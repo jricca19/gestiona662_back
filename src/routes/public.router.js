41a54fdaae8181a93624b77825d84baa783d5fea
Joel <joelriccaalvarez@gmail.com>
2025-12-03 16:39:47 -0300

    mejora de seguridad para tareas cron de endpoint publico

--- src/routes/public.router.js
+++ src/routes/public.router.js
@@ -5,8 +5,7 @@ const fs = require("fs");
 
 const publicRouter = express.Router();
 
-const { healthController, getDepartmentsController, getDepartmentController, getSchoolsSelectController, postSchoolController } = require("../controllers/public.controller");
-const { expirePublicationsTask } = require("../jobs/expirePublications.job");
+const { healthController, getDepartmentsController, getDepartmentController, getSchoolsSelectController, postSchoolController, expirePublicationsController } = require("../controllers/public.controller");
 
 // Ruta para servir swagger.json como archivo estático
 publicRouter.get("/swagger/swagger.json", (req, res) => {
@@ -34,20 +33,7 @@ publicRouter.get("/departments", getDepartmentsController);
 publicRouter.get("/departments/:id", getDepartmentController);
 publicRouter.get('/schoolsSelect', getSchoolsSelectController);
 publicRouter.post('/schools', postSchoolController);
-publicRouter.post("/internal/cron/expire-publications", async (req, res) => {
-  try {
-    const vercelCronHeader = req.headers["x-vercel-cron"];
-    if (!vercelCronHeader) {
-      return res.status(401).json({ ok: false, error: "Unauthorized" });
-    }
-
-    const result = await expirePublicationsTask();
-    res.json({ ok: true, ...result });
-  } catch (error) {
-    console.error("Error ejecutando expirePublicationsTask:", error);
-    res.status(500).json({ ok: false, error: error });
-  }
-});
+publicRouter.get("/internal/cron/expire-publications", expirePublicationsController);
 
 module.exports = publicRouter;
 
